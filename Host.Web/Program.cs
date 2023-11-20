195933fc9327abf04b6c9e1b48f8ffacb354e2ba
Chuan Nguyen <Chuan.Nguyen@altsourcesoftware.com>
2023-11-20 17:12:07 +0700

    config serilog with elasticsearch

--- Host.Web/Program.cs
+++ Host.Web/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services
-    .AddSerilogLogging(builder.Configuration)
+    .AddSerilogLogging()
     .AddAsyncProcessing(builder.Configuration, assembliesWithConsumers: [])
     .AddSwagger();
 
