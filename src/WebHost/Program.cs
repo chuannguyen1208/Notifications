17e28b54e2ba8f4869944853469d785f2ee816b3
Chuan Nguyen <Chuan.Nguyen@altsourcesoftware.com>
2023-11-22 15:11:02 +0700

    sample authentication cookie

--- src/WebHost/Program.cs
+++ src/WebHost/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Serilog;
+using System.Security.Claims;
 using Tools.ErrorHandling;
 using Tools.Logging;
 using Tools.Messaging;
@@ -7,13 +9,17 @@ using Tools.Swagger;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy()
-    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+	.LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 builder.Services
-    .AddSwagger()
-    .AddSerilogLogging(builder.Configuration);
+	.AddSwagger()
+	.AddSerilogLogging(builder.Configuration);
 
 builder.Host.UseSerilog();
+
+builder.Services
+	.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+	.AddCookie();
 
 var app = builder.Build();
 
@@ -22,18 +28,38 @@ app.UseErrorHandling();
 app.UseSwagger();
 app.UseSwaggerUI(opts =>
 {
-    var descriptions = app.DescribeApiVersions();
-    foreach (var groupName in descriptions.Select(d => d.GroupName))
-    {
-        opts.SwaggerEndpoint(
-            url: $"/swagger/{groupName}/swagger.json",
-            name: groupName);
-    }
+	var descriptions = app.DescribeApiVersions();
+	foreach (var groupName in descriptions.Select(d => d.GroupName))
+	{
+		opts.SwaggerEndpoint(
+			url: $"/swagger/{groupName}/swagger.json",
+			name: groupName);
+	}
 });
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapGet("/", () => "Ok").WithOpenApi();
+
+app.MapGet("/login", () =>
+{
+	var claims = new List<Claim>
+	{
+		new("id", Guid.NewGuid().ToString()),
+		new("name", "Admin")
+	};
+
+	var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+	var claimsPrinciple = new ClaimsPrincipal(claimsIdentity);
+	var signIn = Results.SignIn(claimsPrinciple, authenticationScheme: CookieAuthenticationDefaults.AuthenticationScheme);
+
+	return signIn;
+});
+
+app.MapGet("/logout", () => Results.SignOut());
 
 app.MapReverseProxy();
 
