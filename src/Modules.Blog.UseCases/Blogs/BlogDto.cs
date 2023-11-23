3edc32d51dbb040082ec5ef8d4d333ef8d7e01b2
Chuan Nguyen <Chuan.Nguyen@altsourcesoftware.com>
2023-11-23 21:53:31 +0700

    refactor

--- src/Modules.Blog.UseCases/Blogs/BlogDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Modules.Blog.UseCases.Blogs;
-public class BlogDto
-{
-	public int Id { get; set; }
-	public required string Name { get; set; }
-	public string Description { get; set; } = string.Empty;
-	public string ImgUrl { get; set; } = string.Empty;
-}
--- src/Modules.Blog/Models/Blog.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Modules.Blog.Models;
-
-public class Blog
-{
-	public int Id { get; set; }
-	public required string Title { get; set; }
-	public string Description { get; set; } = string.Empty;
-	public string ImgUrl { get; set; } = string.Empty;
-}
