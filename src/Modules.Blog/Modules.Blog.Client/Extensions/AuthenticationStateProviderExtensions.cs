cfc8ca20dee37e958af15a8d1f5eba3e5c7fd92a
Chuan Nguyen <Chuan.Nguyen@altsourcesoftware.com>
2023-11-29 21:43:48 +0700

    Config AuthenticationStateProvider

---
+++ src/Modules.Blog/Modules.Blog.Client/Extensions/AuthenticationStateProviderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Modules.Blog.Client.Extensions;
+
+public static class AuthenticationStateProviderExtensions
+{
+	public static async Task<string?> GetUserNameAsync(this AuthenticationStateProvider authenticationStateProvider)
+	{
+		var authState = await authenticationStateProvider.GetAuthenticationStateAsync();
+		var user = authState.User;
+
+		return user.Identity?.Name;
+	}
+}
