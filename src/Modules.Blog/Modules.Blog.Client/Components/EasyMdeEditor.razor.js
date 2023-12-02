b7603543544b48eeaefc97c08d7d47f714d43460
Chuan Nguyen <Chuan.Nguyen@altsourcesoftware.com>
2023-12-02 20:22:07 +0700

    add easyMde

---
+++ src/Modules.Blog/Modules.Blog.Client/Components/EasyMdeEditor.razor.js
@@ -0,0 +1,11 @@
+﻿export class EasyMdeEditor {
+    static easymde;
+
+    static init(element) {
+        this.easymde = new EasyMDE({
+            element: element
+        });
+    }
+}
+
+window.EasyMdeEditor = EasyMdeEditor;
