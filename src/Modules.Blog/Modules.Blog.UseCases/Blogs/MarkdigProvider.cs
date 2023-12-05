a8666fb6a5898c7394e33bae429d128cbc0f26b5
Chuan Nguyen <Chuan.Nguyen@altsourcesoftware.com>
2023-12-05 10:08:00 +0700

    Markdig package

---
+++ src/Modules.Blog/Modules.Blog.UseCases/Blogs/MarkdigProvider.cs
@@ -0,0 +1,22 @@
+﻿using Markdig;
+
+namespace Modules.Blog.UseCases.Blogs;
+public class MarkdigProvider
+{
+	private readonly MarkdownPipeline _markdownPipeline;
+
+
+	public MarkdigProvider()
+	{
+		_markdownPipeline = new MarkdownPipelineBuilder()
+			.UsePipeTables()
+			.UseAdvancedExtensions()
+			.Build();
+	}
+
+	public string ToHtml(string markdown)
+	{
+		var html = Markdown.ToHtml(markdown, _markdownPipeline);
+		return html;
+	}
+}
