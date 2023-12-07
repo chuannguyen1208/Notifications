8fe444f2abfd105abb16cbdf9dc54c7ddb9cf61b
Chuan Nguyen <Chuan.Nguyen@altsourcesoftware.com>
2023-12-07 11:21:34 +0700

    update gulp

--- src/Modules.Blog/Modules.Blog/wwwroot/assets/gulpfile.js
+++ src/Modules.Blog/Modules.Blog/wwwroot/assets/gulpfile.js
@@ -1,77 +1,48 @@
-﻿const gulp = require('gulp');
-const uglify = require('gulp-uglify');
+﻿const rollupStream = require("@rollup/stream");
+const { nodeResolve } = require("@rollup/plugin-node-resolve");
+const commonjs = require("@rollup/plugin-commonjs");
+const source = require("vinyl-source-stream");
+const buffer = require("vinyl-buffer");
+const gulp = require("gulp");
+const uglify = require("gulp-uglify");
+const gulpSass = require("gulp-sass")(require("sass"));
+const cleanCss = require("gulp-clean-css");
+const concat = require("gulp-concat");
 const del = require('del');
-const dartSass = require('sass');
-const gulpSass = require('gulp-sass')(dartSass);
-const concat = require('gulp-concat');
-const cleanCss = require('gulp-clean-css');
 
-const plumber = require('gulp-plumber');
-const buffer = require('vinyl-buffer');
-const source = require('vinyl-source-stream');
+async function clean() {
+    await del(['js', 'css']);
+}
 
-const rollupStream = require('@rollup/stream');
-const commonjs = require('@rollup/plugin-commonjs');
-const { babel } = require('@rollup/plugin-babel');
-const { nodeResolve } = require('@rollup/plugin-node-resolve');
+function js(filename) {
+  const options = {
+    input: `src/js/${filename}`,
+    output: { format: "es", sourcemap: true },
+    plugins: [nodeResolve({ browser: true }), commonjs()],
+  };
+  return rollupStream(options)
+    .pipe(source(filename))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest("js"));
+}
 
-gulp.task("clean", async function () {
-    await del(['js']);
-});
+function css() {
+  return gulp
+    .src("./src/css/*.scss")
+    .pipe(gulpSass())
+    .pipe(concat("app.css"))
+    .pipe(cleanCss())
+    .pipe(gulp.dest("css"));
+}
 
-gulp.task("editorjs", function () {
-    let outputOptions = {
-        sourcemap: true,
-        format: 'es'
-    }
-    
+const editor = () => js('editor.js');
+const common = () => js('common.js');
+const build = gulp.series(clean, css, gulp.parallel(editor, common));
 
-    let stream = rollupStream({
-        input: './src/js/editor.js',
-        output: outputOptions,
-        plugins: [
-            babel({
-                exclude: 'node_modules/**',
-                presets: ['@babel/preset-env'],
-                babelHelpers: 'bundled',
-            }),
-            nodeResolve({
-                browser: true,
-                preferBuiltins: false,
-            }),
-            commonjs({
-                include: ['node_modules/**'],
-                exclude: [],
-                sourceMap: true,
-            }),
-        ],
-    })
-
-    stream = stream.pipe(source('editor.js'))
-        .pipe(buffer())
-        .pipe(plumber())
-        .pipe(uglify());
-
-    return stream.pipe(gulp.dest('js'));
-});
-
-gulp.task("commonjs", function () {
-    return gulp.src('./src/js/common.js')
-        .pipe(babel())
-        .pipe(uglify())
-        .pipe(gulp.dest('js'));
-});
-
-gulp.task('sass', function () {
-    return gulp.src('./src/css/*.scss')
-        .pipe(gulpSass().on('error', gulpSass.logError))
-        .pipe(concat('app.css'))
-        .pipe(cleanCss())
-        .pipe(gulp.dest('css'));
-});
-
-gulp.task('watchEditorjs', function () {
-    gulp.watch('./src/js/editor.js', gulp.series('editorjs'));
-});
-
-gulp.task('default', gulp.series('clean', 'editorjs', 'sass'));
+exports.editor = editor;
+exports.common = common;
+exports.css = css;
+exports.clean = clean;
+exports.build = build;
+exports.default = build;
