4aaaa720021371b5d4071d0b1534c98c1a5777e3
Chuan Nguyen <Chuan.Nguyen@altsourcesoftware.com>
2023-12-08 12:06:04 +0700

    Host navigate using js instead of href

---
+++ src/OAuthService/OAuthService/Components/Pages/Home.razor.js
@@ -0,0 +1,18 @@
+﻿export function onLoad() {
+    const navs = document.querySelectorAll('.nav-portfolio');
+    navs.forEach(nav => {
+        nav.addEventListener('click', e => {
+            e.preventDefault();
+            const href = nav.getAttribute('href');
+            window.location.href = href;
+        });
+    });
+}
+
+export function onUpdate() {
+    console.log('Updated');
+}
+
+export function onDispose() {
+    console.log('Disposed');
+}
